430c8b84fa1a9bb8f00c479711597bf608d7d8db
Mario Andres Salgado Jaque <marisalgado2023@udec.cl>
2024-04-26 19:00:46 -0400

    define enum preciosProductos

--- src/main/java/tarea1/preciosProductos.java
+++ src/main/java/tarea1/preciosProductos.java
@@ -1,4 +1,19 @@
 package tarea1;
 
 public enum preciosProductos {
+    COCACOLA(1000),
+    FANTA(1200),
+    SPRITE(1200),
+    SNICKERS(500),
+    SUPER8(300);
+
+    private int valor;
+    private preciosProductos (int valor){
+        this.valor = valor;
+    }
+
+    public int valorProductos(){
+        return valor;
+    }
+
 }
