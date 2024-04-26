1bc89af3dfa1cc4abef32dfa033459db534bd08d
Santiago Alexander Diaz Barra <carldiaz2023@udec.cl>
2024-04-26 18:28:54 -0400

    Se agrega "Fanta" como bebida

---
+++ src/main/java/tarea1/Fanta.java
@@ -0,0 +1,13 @@
+package tarea1;
+
+public class Fanta extends Bebida {
+    public Fanta(int serie) {
+        super(serie);
+    }
+
+    @Override
+    public String beber() {
+        return "fanta";
+    }
+}
+
