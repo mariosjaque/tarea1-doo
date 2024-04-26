ab22b5ad0bb63c275d78a91abdda5a5bbcffef47
Santiago Alexander Diaz Barra <carldiaz2023@udec.cl>
2024-04-26 17:52:59 -0400

    Creacion de la excepcion "PagoinsuficienteExcepcion"

---
+++ src/main/java/tarea1/PagoInsuficienteExcepcion.java
@@ -0,0 +1,9 @@
+package tarea1;
+
+import java.io.Console;
+
+public class PagoInsuficienteExcepcion extends java.lang.Exception {
+    public PagoInsuficienteExcepcion(String mensaje) {
+        super(mensaje);
+    }
+}
