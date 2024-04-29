d2d441095d6493a907e87ee4b73e026a704837d4
Nachopex <luisignaciomartinezneira@gmail.com>
2024-04-29 10:43:41 -0400

    cambios en el main

--- src/main/java/tarea1/Main.java
+++ src/main/java/tarea1/Main.java
@@ -3,7 +3,6 @@ package tarea1;
 public class Main {
     public static void main(String[] args) throws NoHayProductoException,PagoInsuficienteExcepcion,PagoIncorrectoException{
 
-<<<<<<< HEAD
         int numProductos = 1;
         int prodnum = 5;
         int cualProducto = 1;
@@ -17,37 +16,25 @@ public class Main {
 
         Moneda500 m = null;
 
-        try{
-            exp.comprarProducto(m,prodnum);
-=======
-        Expendedor exp = new Expendedor(2);
+        try {
+            exp.comprarProducto(m, prodnum);
+            Comprador compa = null;
 
-        Moneda m = new Moneda1000();
-        Comprador compa = null;
+            try {
+                compa = new Comprador(m, 5, exp);
+            } catch (NoHayProductoException e) {
+                System.out.println((e.getMessage()));
+                System.exit(1);
+            } catch (PagoInsuficienteExcepcion e) {
+                System.out.println((e.getMessage()));
+                System.exit(1);
+            } catch (PagoIncorrectoException e) {
+                System.out.println((e.getMessage()));
+                System.exit(1);
+            }
+            Comprador comp = new Comprador(m, cualProducto, exp);
+            System.out.println(compa.queCompraste());
+            System.out.println(compa.cuantoVuelto());
 
-        try{
-            compa = new Comprador(m, 5, exp);
->>>>>>> 9070da5144db23be3c04fda0e9207a2609e9d56b
         }
-        catch (NoHayProductoException e){
-            System.out.println((e.getMessage()));
-            System.exit(1);
-        }
-        catch (PagoInsuficienteExcepcion e){
-            System.out.println((e.getMessage()));
-            System.exit(1);
-        }
-        catch (PagoIncorrectoException e){
-            System.out.println((e.getMessage()));
-            System.exit(1);
-        }
-<<<<<<< HEAD
-        Comprador comp = new Comprador(m,cualProducto,exp);
-=======
-        System.out.println(compa.queCompraste());
-        System.out.println(compa.cuantoVuelto());
->>>>>>> 9070da5144db23be3c04fda0e9207a2609e9d56b
-
-
-    }
 }
