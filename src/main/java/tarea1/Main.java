3bcf55f8e3d01a421106455ead6cee5e7b0b5830
Nachopex <luisignaciomartinezneira@gmail.com>
2024-04-29 10:37:02 -0400

    Modificaciones al main

--- src/main/java/tarea1/Main.java
+++ src/main/java/tarea1/Main.java
@@ -3,19 +3,21 @@ package tarea1;
 public class Main {
     public static void main(String[] args) throws NoHayProductoException,PagoInsuficienteExcepcion,PagoIncorrectoException{
 
-
+        int numProductos = 1;
+        int prodnum = 5;
+        int cualProducto = 1;
         try{
-            Expendedor exp = new Expendedor(1);
+            Expendedor exp = new Expendedor(numProductos);
         } catch (NoHayProductoException e) {
             System.out.println((e.getMessage()));
             System.exit(1);
         }
-        Expendedor exp = new Expendedor(7);
+        Expendedor exp = new Expendedor(numProductos);
 
         Moneda500 m = null;
 
         try{
-            exp.comprarProducto(m,5);
+            exp.comprarProducto(m,prodnum);
         }
         catch (NoHayProductoException e){
             System.out.println((e.getMessage()));
@@ -29,7 +31,7 @@ public class Main {
             System.out.println((e.getMessage()));
             System.exit(1);
         }
-        Comprador comp = new Comprador(m,1,exp);
+        Comprador comp = new Comprador(m,cualProducto,exp);
 
 
     }
